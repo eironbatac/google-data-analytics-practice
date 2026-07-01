2861c555b4c2648ab315213aa495b74e40c3f1e4
Eiron Batac <bataceiron19@gmail.com>
2026-07-01 20:47:49 +0800

    Add first Python practice

---
+++ Python/01_Basics/01_python_basics.py
@@ -0,0 +1,25 @@
+"""
+Google Data Analytics Certificate
+Python Practice
+
+Lesson 01
+Author: Eiron Batac
+"""
+
+print("Hello, Data Analytics!")
+
+name = "Eiron Batac"
+career_goal = "Data Analyst"
+
+print(f"My name is {name}.")
+print(f"My goal is to become a {career_goal}.")
+
+numbers = [10, 20, 30, 40, 50]
+
+print("\nNumbers:")
+for number in numbers:
+    print(number)
+
+average = sum(numbers) / len(numbers)
+
+print(f"\nAverage: {average}")
