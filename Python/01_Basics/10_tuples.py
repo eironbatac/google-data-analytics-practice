fab3279fa1161834459ca891b8da4669719afcb2
Eiron Batac <bataceiron19@gmail.com>
2026-07-09 21:26:53 +0800

    Practice Python tuples

---
+++ Python/01_Basics/10_tuples.py
@@ -0,0 +1,22 @@
+"""
+-------------------------------------------------------
+Lesson 10
+Topic: Tuples
+-------------------------------------------------------
+"""
+
+months = (
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+)
+
+print("Months")
+
+for month in months:
+    print(month)
+
+print("\nFirst Month:", months[0])
+print("Last Month:", months[-1])
