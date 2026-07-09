a078265f5e7f76a515a8087c26229b42ec4fff76
Eiron Batac <bataceiron19@gmail.com>
2026-07-09 21:30:24 +0800

    Practice Python exception handling

---
+++ Python/01_Basics/12_exception_handling.py
@@ -0,0 +1,21 @@
+"""
+-------------------------------------------------------
+Lesson 12
+Topic: Exception Handling
+-------------------------------------------------------
+"""
+
+try:
+    number = int(input("Enter a number: "))
+    result = 100 / number
+
+    print("Result:", result)
+
+except ZeroDivisionError:
+    print("You cannot divide by zero.")
+
+except ValueError:
+    print("Please enter a valid number.")
+
+finally:
+    print("Program Finished.")
